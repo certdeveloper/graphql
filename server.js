1b6aaf0141df156d1ad076d3a8156d1334448a32
certdeveloper <whitestar0713@gmail.com>
2023-10-01 22:26:27 +0800

    add graphql with args

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express")
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema } = require("graphql");
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+    rollDice(numDice: Int!, numSides: Int): [Int]
+  }
+`);
+
+var root = {
+  hello: () => {
+    return "hellowWOrld"
+  },
+  rollDice: (args) => {
+    return [args.numDice + args.numSides]
+  }
+}
+
+var app = express()
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
+
+app.listen(4000)
+console.log("Running")
